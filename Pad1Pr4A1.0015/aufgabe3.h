f49c294a8f58240a6cc4a6e3a4b152567ef49217
StellaW24 <stellaw24@ymail.com>
2018-12-06 14:55:00 +0100

    string im header nicht funktionstüchtig

--- Pad1Pr4A1.0015/aufgabe3.h
+++ Pad1Pr4A1.0015/aufgabe3.h
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <string>
+#include "string.h"
+//#include <string>
+
 #ifndef AUFGABE3_H
 #define AUFGABE3_H
 
 #endif // AUFGABE3_H
 
+struct listPowerProvider {
+    String name;
+    double monthly;
+    double kWH;
+    int bonus;
+}static list[6];
+
 double calculate(double use, double kwh, double monthly, int bonus);
 
-void sort(struct listPowerProvider list[6], double use);
+void sort(listPowerProvider list[], double use);
 
-//void search(string name, struct listPowerProvider list[6]);   //kennt string nicht?!
+void search(string name, struct listPowerProvider list[]);
